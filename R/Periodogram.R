7f4619695f7363b45f271c7a029044936699cd9f
BenjRi96 <68151853+BenjRi96@users.noreply.github.com>
2020-08-18 11:24:04 +0200

    update

--- R/Periodogram.R
+++ R/Periodogram.R
@@ -1,14 +1,15 @@
 #' The Periodogram
 #'
-#' The periodogram estimates the power spectral density of a signal (e.g. a time
-#' series or a series of observations). Note that the periodogram is not a consistent estimator.
+#' The Periodogram estimates the power spectral density of a signal (e.g. a time
+#' series or a series of observations). Note that the Periodogram is not a consistent estimator.
 #' 
 #' @param ts A numeric vector or a structur of class arma which contains the
 #'   time series that should be analysed.
-#' @return A vector which contains the estimated power spectral density of the signal or time series \code{ts}.
+#' @return A vector which contains the values of the Periodogram of the signal
+#'   or the time series \code{ts} at the Fourier frequencys.
 #' @examples
-#' n<- 1:42
-#' x <- rnorm(1:42) + 2*sin(2*pi*n)
+#' n<- 1:1000
+#' x <- rnorm(1:1000) + 2*sin(2*pi*n)
 #' periodogram(x)
 #' @export
 periodogram <- function(ts) {
